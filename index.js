6755031cc88fa27dbf58026d4c3bf770eca879c6
Franco Sarachu <fsarachu@gmail.com>
2018-05-18 13:00:51 -0300

    Install and setup cors

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ if (process.env.NODE_ENV !== 'production') {
 // Modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const initDB = require('./db');
 const registerModules = require('./modules');
 const app = express();
 
 // Setup middleware
+app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
