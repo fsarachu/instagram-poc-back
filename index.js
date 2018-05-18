b85088ac9fcfdc96ad11a212c05b6acdd8921dcb
Franco Sarachu <fsarachu@gmail.com>
2018-05-18 10:13:49 -0300

    Express Hello World

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
