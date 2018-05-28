f48a65460ef6901362388960c6280cf3882ba989
Franco Sarachu <fsarachu@gmail.com>
2018-05-28 17:09:56 -0300

    Refactor User mongoose schema and model

--- db/models/User.js
+++ db/models/User.js
@@ -1,44 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    },
     facebookProvider: {
-        type: {
-            id: String,
-            token: String
-        },
-        // select: false
+        id: String,
+        scopes: Array,
+        shortLivedToken: String,
+        longLivedToken: String,
     }
 });
-
-userSchema.statics.upsertFbUser = function(accessToken, refreshToken, profile, cb) {
-    return this.findOne({'facebookProvider.id': profile.id}, (err, user) => {
-        // no user was found, lets create a new one
-        if (!user) {
-            const newUser = new this({
-                email: profile.emails[0].value,
-                facebookProvider: {
-                    id: profile.id,
-                    token: accessToken
-                }
-            });
-
-            newUser.save(function(error, savedUser) {
-                if (error) {
-                    console.log(error);
-                }
-                return cb(error, savedUser);
-            });
-        } else {
-            return cb(err, user);
-        }
-    });
-};
 
 module.exports = mongoose.model('User', userSchema);
