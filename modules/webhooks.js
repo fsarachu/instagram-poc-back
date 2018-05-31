4914f77b5aadbdb63b37ee897ecdb1b751c7fdbc
Franco Sarachu <fsarachu@gmail.com>
2018-05-31 17:50:33 -0300

    Instagram Webhooks mocked WIP

--- modules/webhooks.js
+++ modules/webhooks.js
@@ -1,7 +1,6 @@
 const Router = require('express').Router;
 const router = new Router();
-
-const receivedUpdates = []
+const Account = require('../db/models/Account');
 
 function verifyRequest(req, res) {
     if (req.param('hub.mode') == 'subscribe' && req.param('hub.verify_token') == process.env.FB_WEBHOOK_TOKEN) {
@@ -22,10 +21,87 @@ function processFacebookEvent(req, res) {
 
     console.log('request header X-Hub-Signature validated');
 
-    // Process the Facebook updates here
-    receivedUpdates.unshift(req.body);
-
     res.sendStatus(200);
+}
+
+
+function findAccount(instagramAccountId) {
+    Account.findOne({"instagramProfile.id": instagramAccountId})
+        .then(account => {
+            if (!account) {
+                const message = `Instagram Account "${instagramAccountId}" not found`;
+                console.error(message);
+                throw new Error(message);
+            }
+
+            return account;
+        });
+}
+
+function processInstagramPostComment(instagramAccountId, commentId, commentText) {
+    // Mocked, but should query this node: https://developers.facebook.com/docs/instagram-api/reference/comment#reading
+    const commentMock = {
+        id: commentId,
+        username: 'Someone',
+        date: Date.now(),
+        text: commentText,
+        likeCount: 0,
+        mediaId: '17909981962114643',
+    };
+
+    findAccount(instagramAccountId)
+        .then(acc => {
+            const activityItem = {
+                event: 'post_comment',
+                data: commentMock
+            };
+
+            acc.instagramProfile.activity.unshift(activityItem);
+            return acc.save();
+        });
+}
+
+function processInstagramCommentMention(instagramAccountId, commentId, mediaId) {
+    // Mocked, but should query this node: https://developers.facebook.com/docs/instagram-api/reference/user/mentioned_comment
+    const mentionMock = {
+        username: 'Someone',
+        timestamp: Date.now(),
+        likesCount: 0,
+        text: '@example This is some dummy text',
+    };
+
+    findAccount(instagramAccountId)
+        .then(acc => {
+            const activityItem = {
+                event: 'comment_mention',
+                data: mentionMock
+            };
+
+            acc.instagramProfile.activity.unshift(activityItem);
+            return acc.save();
+        });
+}
+
+function processInstagramCaptionMention(instagramAccountId, mediaId) {
+    // Mocked, but should query this node: https://developers.facebook.com/docs/instagram-api/reference/user/mentioned_media
+    const mentionMock = {
+        commentId: "17841405309211844",
+        mediaId: "17918195224117851",
+        caption: "@upshow This is a mocked caption!",
+        mediaType: "IMAGE",
+        mediaUrl: 'https://s3.amazonaws.com/spotlights.upshow.tv/7bcc7f1b-e707-418c-b8be-f12a8246d4c4_nice-background.png',
+    };
+
+    findAccount(instagramAccountId)
+        .then(acc => {
+            const activityItem = {
+                event: 'caption_mention',
+                data: mentionMock
+            };
+
+            acc.instagramProfile.activity.unshift(activityItem);
+            return acc.save();
+        });
 }
 
 function processInstagramEvent(req, res) {
@@ -33,15 +109,42 @@ function processInstagramEvent(req, res) {
     console.log(req.body);
 
     // Process the Instagram updates here
-    receivedUpdates.unshift(req.body);
+    const entries = req.body.entry;
 
-    return res.sendStatus(200);
+    const promises = entries.map(entry => {
+        const instagramAccountId = entry.id;
+        const changeField = entry.changes[0].field;
+        const changeValue = entry.changes[0].value;
+
+        let promise;
+
+        if (changeField === 'comments') {
+            promise = processInstagramPostComment(instagramAccountId, changeValue.id, changeValue.text);
+        } else if (changeField === 'mentions') {
+            if (changeValue.comment_id) {
+                promise = processInstagramCommentMention(instagramAccountId, changeValue.comment_id, changeValue.media_id);
+            } else {
+                promise = processInstagramCaptionMention(instagramAccountId, changeValue.media_id);
+            }
+        } else {
+            console.warn(`Unexpected webhook field "${changeField}"`);
+            promise = Promise.resolve();
+        }
+
+        return promise;
+    });
+
+    Promise.all(promises)
+        .then(() => {
+            console.log('Successfully processed Instagram Webhook');
+            return res.sendStatus(200);
+        })
+        .catch(e => {
+            console.error('Error processing Instagram Webhook: ', e);
+            return res.sendStatus(500);
+        });
+
 }
-
-
-router.get('/webhooks', function(req, res) {
-    res.json(receivedUpdates);
-});
 
 router.get(['/webhooks/facebook', '/webhooks/instagram'], verifyRequest);
 
